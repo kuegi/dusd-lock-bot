ec44c5721eed1fd2b102e487671237a196ebd137
kuegi <29012906+kuegi@users.noreply.github.com>
2023-12-05 14:12:25 +0100

    fix logic errors

--- DUSDLock.sol
+++ DUSDLock.sol
@@ -57,17 +57,22 @@ contract DUSDLock {
 
     function availableRewards(address addr,uint batch) public view returns(uint256 rew) {
         LockEntry[] storage entries= investments[addr];
-        require(entries.length <= batch,"DUSDLock: batch not found for this address");
+        require(entries.length > batch,"DUSDLock: batch not found for this address");
         uint256 addedRewPerDeposit= rewardsPerDeposit - entries[batch].initialRewardsPerDeposit;
         uint256 remainingFunds= entries[batch].amount - entries[batch].withdrawn;
-        return addedRewPerDeposit*remainingFunds/1e18 - entries[batch].claimedRewards;
+        uint256 totalRewardsForFunds= addedRewPerDeposit*remainingFunds/1e18;
+        if(totalRewardsForFunds > entries[batch].claimedRewards) {
+            return totalRewardsForFunds - totalRewardsForFunds;
+        } else {
+            return  0;
+        }
     }
 
-    function batchesInAddress(address addr) public view returns(uint count) {
+    function batchesInAddress(address addr) external view returns(uint count) {
         count= investments[addr].length;
     }
 
-    function earliestUnlock(address addr) public view returns(uint256 timestamp,uint batchId) {
+    function earliestUnlock(address addr) external view returns(uint256 timestamp,uint batchId) {
         LockEntry[] storage entries= investments[addr];
         timestamp = block.timestamp + lockupPeriod;
         batchId = 0;
@@ -80,20 +85,19 @@ contract DUSDLock {
     }
 
     function lockup(uint256 funds) external {
-        require(totalInvest+funds <= totalInvestCap,"Total invest cap reached");
-        require(funds >= 10*(10**18),"min funds not meet"); //to prevent spam of low fund addresses which drives up gas price on reward distribution
+        require(totalInvest+funds <= totalInvestCap,"DUSDLock: Total invest cap reached");
         coin.transferFrom(msg.sender,address(this),funds);
         LockEntry[] storage entries= investments[msg.sender];
         
         entries.push(LockEntry(msg.sender,funds,0,block.timestamp+lockupPeriod,rewardsPerDeposit,0));
         totalInvest += funds;
 
-        emit DepositAdded(msg.sender, funds, totalInvest);
+        emit DepositAdded(msg.sender, funds, currentTvl());
     }
 
     function withdraw(uint batchId) external returns(uint256 withdrawAmount) {
         LockEntry[] storage entries= investments[msg.sender];
-        require(entries.length <= batchId,"DUSDLock: batch not found for this address");
+        require(entries.length > batchId,"DUSDLock: batch not found for this address");
         require(availableRewards(msg.sender,batchId) == 0,"DUSDLock: claim rewards before withdraw");
         
         LockEntry storage entry= entries[batchId];
@@ -105,10 +109,10 @@ contract DUSDLock {
         entry.withdrawn+= withdrawAmount;
         coin.transfer(msg.sender, withdrawAmount);
 
-        emit Withdrawal(msg.sender, withdrawAmount, totalInvest-totalWithdrawn);
+        emit Withdrawal(msg.sender, withdrawAmount, currentTvl());
     }
 
-    function claimRewards(uint batchId) internal returns(uint256 claimed) {
+    function claimRewards(uint batchId) external returns(uint256 claimed) {
         claimed= availableRewards(msg.sender,batchId);
         require(claimed > 0,"DUSDLock: no rewards to claim");
         LockEntry storage entry= investments[msg.sender][batchId];
@@ -119,7 +123,7 @@ contract DUSDLock {
     }
 
     function addRewards(uint256 rewardAmount) external {
-        require(totalInvest-totalWithdrawn > 0,"can't distribute rewards on empty TVL");
+        require(totalInvest-totalWithdrawn > 0,"can not distribute rewards on empty TVL");
         coin.transferFrom(msg.sender, address(this), rewardAmount);
         totalRewards += rewardAmount;
 
