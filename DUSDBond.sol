d991695a876268f25c8e5c840bc2f4fbfec74e70
kuegi <29012906+kuegi@users.noreply.github.com>
2024-01-03 16:53:59 +0100

    improved SVG, removed unnecessary function

--- DUSDBond.sol
+++ DUSDBond.sol
@@ -18,6 +18,11 @@ contract Bond is ERC721Enumerable, Ownable {
 
     BondManager public immutable manager;
 
+    //TODO: define thresholds, just for the SVG of the NFT.
+    uint constant CATEGORY_1_THRESHOLD = 100 ether;
+    uint constant CATEGORY_2_THRESHOLD = 1_000 ether;
+    uint constant CATEGORY_3_THRESHOLD = 10_000 ether;
+
     string[12] months= ['January','February','March','April', 'May', 
                         'June', 'July', 'August', 'September', 'October', 
                         'November', 'December'];
@@ -52,21 +57,59 @@ contract Bond is ERC721Enumerable, Ownable {
       return string(abi.encodePacked(Strings.toString(day),' ',months[month-1],' ',Strings.toString(year)));
     }
 
+    function getCategory(uint256 _tokenId) public view returns(uint) {
+        LockEntry memory dusdBondData = manager.getBatchData(_tokenId);
+        return dusdBondData.amount >= CATEGORY_3_THRESHOLD ? 3 : 
+                        (dusdBondData.amount >= CATEGORY_2_THRESHOLD ? 2 :
+                        (dusdBondData.amount >= CATEGORY_1_THRESHOLD ? 1 : 0));
+    }
 
     function buildImage(uint256 _tokenId) public view returns(string memory) {
         LockEntry memory dusdBondData = manager.getBatchData(_tokenId);
         uint256 lockPeriodYears= manager.lockupPeriod()/(86400*365);
-        //uint256 rewards= manager.availableRewards(_tokenId);
-        //TODO: add colored "mamba" based on locked amount
+
+        uint category= getCategory(_tokenId);
+
+        string memory background= '<circle cx="64" cy="64" r="64" fill="#ffccef"/>';
+        string memory bkgEnd= "";
+        string memory extraStyle= "";
+        string memory textFill= "#FF00AF";
+        string memory snakeFill= "#ffa3e2";
+        if(category == 1) {
+            extraStyle= "div { background: conic-gradient(from 180deg at 50% 50%, #ECC059 12.109362334012985deg, #F4CB6B 31.872142553329468deg, #D79D00 59.2612087726593deg, #D79D00 77.21996605396271deg, #F7D178 110.62355875968933deg, #FFE9AF 129.1939401626587deg, #FFEAB0 154.6223545074463deg, #F5CA67 185.27775049209595deg, #CE9900 199.67872381210327deg, #C99200 218.69185209274292deg, #D6A730 238.1258726119995deg, #FFEAB4 258.9100742340088deg, #FFE9B4 275.6265449523926deg, #C99200 304.00485277175903deg, #CA9300 325.696177482605deg, #ECC059 360deg); width: 128px; height: 128px; display:block; border-radius: 64px;}";
+            background= '<foreignObject width="100%" height="100%"><div xmlns="http://w3.org/1999/xhtml"></div></foreignObject><g style="mix-blend-mode:color-burn">';
+            bkgEnd= "</g>";
+            textFill= "#CFA949";
+            snakeFill= "#FEEAB6";
+        }
+        if(category == 2) {
+            extraStyle= "div {background: conic-gradient(from 180deg at 50% 50%, #191919 11.516567319631577deg, #181818 29.406597018241882deg, #020202 61.5218710899353deg, #020202 77.21996605396271deg, #111 111.62785291671753deg, #1C1C1C 126.60219669342041deg, #2B2B2B 147.47865557670593deg, #111 188.5664176940918deg, #111 199.67872381210327deg, #0D0D0D 238.1258726119995deg, #111 258.9100742340088deg, #3C3C3C 276.8317151069641deg, #111 304.00485277175903deg, #111 325.696177482605deg, #191919 345.4662036895752deg);width: 128px;height: 128px; display:block; border-radius: 64px;}";
+            background= '<foreignObject width="100%" height="100%"><div xmlns="http://w3.org/1999/xhtml"></div></foreignObject><g style="mix-blend-mode:soft-light">';
+            bkgEnd= "</g>";
+            textFill= "#ffffff";
+            snakeFill= "#aaaaaa";
+        }
+        if(category == 3) {
+            extraStyle= "div {background: conic-gradient(from 180deg at 50% 50%, #91E9E7 0deg, #71FFD8 18.622738122940063deg, #7FFFA8 76.90795004367828deg, #6AFFDB 85.92861592769623deg, #64FFE1 91.02773666381836deg, #96FFC0 117.44513511657715deg, #63FFE4 128.4911048412323deg, #71FAE6 139.50835347175598deg, #8EE9E7 153.5158896446228deg, #A8DAEC 174.38389420509338deg, #B1D3EE 206.88481092453003deg, #C5C3EE 217.9077672958374deg, #ED9FF5 275.6265449523926deg, #9AE3EA 304.00485277175903deg, #B2D1ED 335.6388473510742deg, #91E9E7 360deg); width: 128px; height: 128px; display:block; border-radius: 64px; }";
+            background= '<foreignObject width="100%" height="100%"><div xmlns="http://w3.org/1999/xhtml"></div></foreignObject><g style="mix-blend-mode:color-burn">';
+            bkgEnd= "</g>";
+            textFill= "#307CC1";
+            snakeFill= "#85E4DF";
+        }
+
         return Base64.encode(bytes(
             abi.encodePacked(
                 '<svg width="100%" height="100%" viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">',
                 '<defs><path id="curve" d="M34.67 115.77C43.4 120.82 53.54 123.7 64.36 123.7C97.17 123.7 123.71 97.15 123.71 64.35C123.71 31.55 97.16 5 64.35 5C31.54 5 5 31.55 5 64.35C5 75.17 7.89 85.31 12.93 94.04"/>',
-                ' <style> @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@600"); @keyframes rotate {0%    { rotate: 0; } 100%  { rotate: 360deg; } } text {font-size: 0.48rem; font-family: "Montserrat", sans-serif; font-weight: 600; letter-spacing: 0.02rem; transform-origin: 50% 50%; animation: rotate 24s infinite linear; } </style>',
-                '</defs>',
-                '<circle cx="64" cy="64" r="64" fill="#ffccef"/>'
-                '<path d="M56.03 49.85L63.34 45.63L127.94 62.94C127.88 59.06 127.49 55.26 126.77 51.58L69.15 36.13L78.42 1.54C74.97 0.74 71.41 0.22 67.77 0L58.03 36.36L50.7 40.59L49.97 41.03C42.87 45.63 40.56 55.04 44.84 62.45C49.26 70.11 59.05 72.73 66.71 68.31C69.26 66.84 72.51 67.71 73.98 70.26C75.45 72.81 74.58 76.07 72.03 77.54L63.79 82.3L0 65.24C0.08 69.13 0.49 72.94 1.24 76.63L58.01 91.82L48.85 126.07C52.29 126.9 55.83 127.46 59.46 127.71L69.14 91.56L77.37 86.81L78.09 86.37C85.18 81.76 87.5 72.36 83.22 64.95C78.8 57.3 69.01 54.67 61.36 59.09C58.81 60.56 55.55 59.69 54.08 57.14C52.61 54.59 53.48 51.33 56.03 49.86V49.85Z" fill="#ffa3e2"/>',
-                '<text width="96" fill="#FF00AF"><textPath xlink:href="#curve">',to2DigitString(dusdBondData.amount,1e18),' DUSD-',Strings.toString(lockPeriodYears),'-year-BOND | until ',formatTimestamp(dusdBondData.lockedUntil),'</textPath></text>',
+                '<style> @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@600"); @keyframes rotate {0% { rotate: 0; } 100% { rotate: 360deg; } } text {font-size: 0.48rem; font-family: "Montserrat", sans-serif; font-weight: 600; letter-spacing: 0.02rem; transform-origin: 50% 50%; animation: rotate 24s infinite linear; }',
+                extraStyle,
+                '</style></defs>',
+                background,
+                '<path d="M56.03 49.85L63.34 45.63L127.94 62.94C127.88 59.06 127.49 55.26 126.77 51.58L69.15 36.13L78.42 1.54C74.97 0.74 71.41 0.22 67.77 0L58.03 36.36L50.7 40.59L49.97 41.03C42.87 45.63 40.56 55.04 44.84 62.45C49.26 70.11 59.05 72.73 66.71 68.31C69.26 66.84 72.51 67.71 73.98 70.26C75.45 72.81 74.58 76.07 72.03 77.54L63.79 82.3L0 65.24C0.08 69.13 0.49 72.94 1.24 76.63L58.01 91.82L48.85 126.07C52.29 126.9 55.83 127.46 59.46 127.71L69.14 91.56L77.37 86.81L78.09 86.37C85.18 81.76 87.5 72.36 83.22 64.95C78.8 57.3 69.01 54.67 61.36 59.09C58.81 60.56 55.55 59.69 54.08 57.14C52.61 54.59 53.48 51.33 56.03 49.86V49.85Z"',
+                ' fill="',snakeFill,'"/>',
+                '<text width="96" fill="',textFill,'"><textPath xlink:href="#curve">',to2DigitString(dusdBondData.amount,1e18),' DUSD-',Strings.toString(lockPeriodYears),'-year-BOND | until ',formatTimestamp(dusdBondData.lockedUntil),
+                '</textPath></text>',
+                bkgEnd,
                 '</svg>'
             )
         ));
@@ -83,6 +126,7 @@ contract Bond is ERC721Enumerable, Ownable {
                             '"amount":',Strings.toString(dusdBondData.amount),',',
                             '"lockedUntil":',Strings.toString(dusdBondData.lockedUntil),',',
                             '"availableRewards":',Strings.toString(rewards),',',
+                            '"category":',Strings.toString(getCategory(_tokenId)),',',
                             '"image": "data:image/svg+xml;base64,',buildImage(_tokenId),
                             '"}')))));
     }
@@ -224,22 +268,6 @@ contract BondManager is Ownable, ReentrancyGuard {
         return allFunds;
     }
 
-    function earliestUnlock(address addr) external view returns(uint256 timestamp,uint earliestBatchId) {
-        timestamp = block.timestamp + lockupPeriod;
-        earliestBatchId = 0;
-        uint256 tokens= bondToken.balanceOf(addr);
-        if( tokens == 0) {
-            revert BondsNoBondsInAddress(addr);
-        }
-        for(uint idx = 0; idx < tokens; ++idx) {
-            uint256 batchId= bondToken.tokenOfOwnerByIndex(addr,idx);
-            if(investments[batchId].lockedUntil < timestamp) {
-                timestamp= investments[batchId].lockedUntil;
-                earliestBatchId= batchId;
-            }
-        }
-    }
-
     // ------ Bond methods -----
 
     function lockup(uint256 funds) external nonReentrant {
@@ -257,6 +285,21 @@ contract BondManager is Ownable, ReentrancyGuard {
 
     function withdraw(uint batchId) external nonReentrant ownedBatch(batchId) returns(uint256 withdrawAmount) {
         withdrawAmount= _withdrawBatch(batchId);
+    }
+
+    function withdrawAllMyAvailableBatches() external nonReentrant returns(uint256 total) {
+        total= 0;
+        uint256 tokens= bondToken.balanceOf(msg.sender);
+        for(uint idx = 0; idx < tokens; ++idx) {
+            uint256 batchId= bondToken.tokenOfOwnerByIndex(msg.sender, idx);
+            LockEntry storage entry= investments[batchId];
+            if(entry.lockedUntil <= block.timestamp || exitCriteriaTriggered) {
+                total += _withdrawBatch(batchId);
+            }   
+        }
+        if(total == 0) {
+            revert BondsNothingWithdrawable();
+        }
     }
 
     function _withdrawBatch(uint batchId) internal returns(uint256 withdrawAmount) {
@@ -318,20 +361,6 @@ contract BondManager is Ownable, ReentrancyGuard {
     }
 
 
-    function withdrawAllMyAvailableBatches() external nonReentrant returns(uint256 total) {
-        total= 0;
-        uint256 tokens= bondToken.balanceOf(msg.sender);
-        for(uint idx = 0; idx < tokens; ++idx) {
-            uint256 batchId= bondToken.tokenOfOwnerByIndex(msg.sender, idx);
-            LockEntry storage entry= investments[batchId];
-            if(entry.lockedUntil <= block.timestamp || exitCriteriaTriggered) {
-                total += _withdrawBatch(batchId);
-            }   
-        }
-        if(total == 0) {
-            revert BondsNothingWithdrawable();
-        }
-    }
 
     //this method is used to add rewards to the bonds. can be called by anyone, 
     // but is expected to be called by the native bot transfering the swapped DFI rewards (natively swapped to DUSD) in.
